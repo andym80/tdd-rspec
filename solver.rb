8fd8ab1512f779d319792e0b9f075b95b52da1fb
Andy Menutti <andymenutti@gmail.com>
2022-11-10 16:58:23 -0300

    right method :factorial Ok

--- solver.rb
+++ solver.rb
@@ -1,15 +1,11 @@
-# const class Solver(factorial)
-
-#   def initialize(factorial)
-# 	@factorial = factorial
-#   end
-
-#   def factorial(number)
-# 	if number.zero?
-# 	1
-# 	elsif number.negative?
-# 	‘No Negative Integers’
-# 	else
-# 	number * factorial(number - 1)
-# 	end
-# end
+class Solver
+  def factorial(number)
+    if number.zero?
+      1
+    elsif number.negative?
+      ‘No Negative Integers’
+    else
+      number * factorial(number - 1)
+    end
+  end
+end
